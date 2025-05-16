a45070fdb9a646f6750fb68247d0cce13f5c03c6
emre.yigit <emreygt01@icloud.com>
2025-05-16 13:26:00 +0300

    fixed base

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-   base: '/goit-react-hw-03/',
+  base: "/",
   plugins: [react()],
-})
+});
